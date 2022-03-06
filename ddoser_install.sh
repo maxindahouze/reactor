a6a8088130fb3736e47e364025fc481eee2d50cb
maxindahouze <maxindahouze@ya.ru>
2022-03-06 13:27:23 +0200

    Update ddoser_install.sh

--- ddoser_install.sh
+++ ddoser_install.sh
@@ -1,5 +1,5 @@
-sudo apt update
-sudo apt install -y git python3 python3-pip
+sudo apt-get update
+sudo apt-get install -y git python3 python3-pip
 git clone https://github.com/taransergey/ddoser.git
 cd ddoser/
 pip install -r requirements.txt
